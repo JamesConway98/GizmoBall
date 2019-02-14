1d06aed77569b0ad2aa1d26c8383f4db124c685e
Kevin Green (Student) <ehb16138@ds.strath.ac.uk>
2019-02-14 15:33:27 +0000

    Added Flipper

---
+++ FLipper/src/model/Gizmo.java
@@ -0,0 +1,24 @@
+package model;
+
+import physics.Circle;
+import physics.LineSegment;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public interface Gizmo {
+    public void setHitbox();
+    public int getX();
+    public int getY();
+    public int getLength();
+    public ArrayList<LineSegment> getEdges();
+    public ArrayList<Circle> getVertices();
+    public void clearCollisions();
+    public Color getColour();
+    public void setColour(Color colour);
+    public int getRotation();
+    public void setRotation(int rotation);
+    public void rotateClockwise();
+    public void rotateAnticlockwise();
+
+}
