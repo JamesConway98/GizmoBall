d2228ea399ba06d211a5cbe32025704c7160b154
Jordan <pkb16162@uni.strath.ac.uk>
2019-03-12 10:13:47 +0000

    Added GUI layouts

---
+++ Gizmoball/src/view/AddPan.java
@@ -0,0 +1,33 @@
+package view;
+
+import javax.swing.*;
+
+public class AddPan {
+    private JTabbedPane tabbedPane1;
+    private JPanel AddPanel;
+    private JPanel EditPanel;
+    private JPanel SettingsPanel;
+    private JButton circleButton;
+    private JButton triangleButton;
+    private JButton squareButton;
+    private JButton leftFlipperButton;
+    private JButton rightFlipperButton;
+    private JLabel Flippers;
+    private JTextField inVel;
+    private JTextField inDir;
+    private JButton ballButton;
+    private JButton absorberButton;
+    private JButton moveButton;
+    private JButton rotateCCWButton;
+    private JButton rotateCWButton;
+    private JButton deleteButton;
+    private JButton addConnectionButton;
+    private JButton removeConnectionButton;
+    private JButton addKeyConnectionButton;
+    private JButton removeKeyConnectionButton;
+    private JSlider slider1;
+    private JSlider slider2;
+    private JSlider slider3;
+    private JButton button9;
+    private JLabel gravLabel;
+}
