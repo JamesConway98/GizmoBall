e2b50309d68aa1ad173b6a6c072c7d0e63c6f42d
James Conway <james.conway.2016@uni.strath.ac.uk>
2019-03-18 14:17:52 +0000

    Nice Push

--- Gizmoball/src/view/EditBuildPanel.java
+++ Gizmoball/src/view/EditBuildPanel.java
@@ -19,7 +19,7 @@ public class EditBuildPanel extends JPanel implements Observer {
     private JButton rightRotateButton;
     private JButton deleteButton;
     private JButton addConnectionButton;
-    private JButton removeConnectionButton;
+    private JButton editConnectionButton;
     private JButton editKeyConnectionButton;
     private JButton removeKeyConnectionButton;
 
@@ -64,10 +64,8 @@ public class EditBuildPanel extends JPanel implements Observer {
 
         deleteButton = new JButton("Delete");
         buttons.add(deleteButton);
-        addConnectionButton = new JButton("Add Connection");
-        buttons.add(addConnectionButton);
-        removeConnectionButton = new JButton("Remove Connection");
-        buttons.add(removeConnectionButton);
+        editConnectionButton = new JButton("Edit Connections");
+        buttons.add(editConnectionButton);
         editKeyConnectionButton = new JButton("Edit Key Connections");
         buttons.add(editKeyConnectionButton);
 
@@ -156,17 +154,19 @@ public class EditBuildPanel extends JPanel implements Observer {
         gc.gridwidth = 1;
 
         gc.weightx = 0.5;
-        gc.weighty = 0.1;
+        gc.weighty = 0;
 
         gc.gridx = 0;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+
         gc.anchor = GridBagConstraints.LINE_START;
-        gc.insets = new Insets(0,0,0,0);
-        add(addConnectionButton, gc);
+        gc.gridwidth = 2;
+        gc.insets = new Insets(30,75,0,75);
 
         gc.gridx = 1;
         gc.anchor = GridBagConstraints.LINE_START;
         gc.insets = new Insets(0, 0,0,0);
-        add(removeConnectionButton, gc);
+        add(editConnectionButton, gc);
 
         /////////////////// Next Row ////////////////////////////
         gc.gridy++;
