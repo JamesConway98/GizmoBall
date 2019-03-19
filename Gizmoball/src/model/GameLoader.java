b49535aacd336284bb23187a507c31ad10989acf
pkb16162 <pkb16162@uni.strath.ac.uk>
2019-03-19 16:31:21 +0000

    fixed Connections bug, changed ball to load coords in terms of L

--- Gizmoball/src/model/GameLoader.java
+++ Gizmoball/src/model/GameLoader.java
@@ -36,6 +36,8 @@ public class GameLoader {
 					by = Float.parseFloat(read.next());
 					xVelo = Double.parseDouble(read.next());
 					yVelo = Double.parseDouble(read.next());
+                    xVelo = xVelo / L - 50;
+                    yVelo = yVelo / L - 50;
 					Ball ball = new Ball(bx, by, xVelo, yVelo);
 					model.addBall(ball);
 				} else if (type.equals("Square") || type.equals("Triangle") || type.equals("Absorber") || type.equals("LeftFlipper") || type.equals("RightFlipper") || type.equals("Circle")) {
